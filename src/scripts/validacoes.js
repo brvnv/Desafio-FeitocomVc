b68302f6ea3f0f10a90745c2e2c137e3db53a773
brvnv <bruno.rdx@metalfan.com>
2021-05-17 00:04:21 -0300

    Adicionadas mais validações para o validador de nome, validador de rg criado.

--- src/scripts/validacoes.js
+++ src/scripts/validacoes.js
@@ -1,4 +1,8 @@
 function validaNome(nome) {
+    if(nome.length > 25) {
+      alert("Nome muito longo.");
+      return false;
+    }
     var regExNumNoNome=/\d/; //procura números no nome
     if(regExNumNoNome.test(nome)==true) {
       alert("Por favor, digite um nome válido para continuar.");
@@ -11,6 +15,15 @@ function validaNome(nome) {
 function validaCPF(cpf) {
     if(cpf.length!=11) {
       alert("Por favor, digite um CPF válido para continuar.");
+      return false;
+    } else {
+      return true;
+    }
+}
+
+function validaRG(rg) {
+    if(rg.length != 9) {
+      alert("Por favor, digite um RG válido para continuar.");
       return false;
     } else {
       return true;
@@ -30,6 +43,7 @@ else {
 function validaDados(){
     if(validaNome(document.dadosConta.nome.value)==false) return false;
     if(validaCPF(document.dadosConta.cpf.value)==false) return false;
+    if(validaRG(document.dadosConta.rg.value)==false) return false;
     if(validaCep(document.dadosConta.cep.value)==false) return false;
     return true;
 }
